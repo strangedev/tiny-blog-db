d0d28c1bb1b0648b492ad939b6a1a62c4a3e1f09
Noah Hummel <n.p.hummel@gmail.com>
2019-03-03 21:15:48 +0100

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+import * as Model from "tiny-blog-model";
+import * as v1alphaBlogEntryView from "./v1alpha/BlogEntry/view";
+import {getVersion} from "./connector/db";
+import * as v1alphaBlogEntryMutation from "./v1alpha/BlogEntry/mutation";
+import * as v1alphaTagView from "./v1alpha/Tag/view";
+
+const v1alpha = {
+    model: {
+        BlogEntry: Model.v1alpha.model.BlogEntry
+    },
+    store: (host, port) => ({
+        getDb: () => getVersion("v1alpha", host, port),
+        BlogEntry: {
+            view: {
+                byTag: v1alphaBlogEntryView.byTag(host, port),
+                newest: v1alphaBlogEntryView.newest(host, port)
+            },
+            mutation: {
+                insert: v1alphaBlogEntryMutation.insert(host, port),
+                update: () => {},
+                remove: v1alphaBlogEntryMutation.remove(host, port)
+            }
+        },
+        Tag: {
+            view: {
+                all: v1alphaTagView.all(host, port)
+            }
+        }
+    })
+};
+
+export {
+    v1alpha
+}
