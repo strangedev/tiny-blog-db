e8669a1dfd0a4de73341db1e8cd76250a4eef46e
Noah Hummel <n.p.hummel@gmail.com>
2019-03-06 11:39:48 +0100

    [TASK] refactor BlogEntry.view.newest to use Future.node

--- src/BlogEntry/view.js
+++ src/BlogEntry/view.js
@@ -6,20 +6,13 @@ import {BlogEntry} from "tiny-blog-model";
 function newest(getDb) {
     return (offset, limit) =>
         getDb().chain(
-            ({db, closeWith}) => Future.Future(
-                (reject, resolve) =>  {
-                    db.collection("BlogEntry")
-                        .find()
-                        .sort({ date: 1 })
-                        .skip(offset)
-                        .limit(limit)
-                        .toArray(
-                            (err, results) => {
-                                if (R.isNil(err)) resolve(results);
-                                else reject(err);
-                            }
-                        )
-                }
+            ({db, closeWith}) => Future.node(
+                done => db.collection("BlogEntry")
+                    .find()
+                    .sort({ date: 1 })
+                    .skip(offset)
+                    .limit(limit)
+                    .toArray(done)
             ).map(
                 entries => R.map(e => BlogEntry.unMarshal(e), entries)
             ).chain(closeWith)
